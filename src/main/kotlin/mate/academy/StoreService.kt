f91ac48fa4d91e4333dd9df8cd287db891932a46
TonyH277 <gajjduktoxa@gmail.com>
2024-08-11 14:48:08 +0300

    implement with

--- src/main/kotlin/mate/academy/StoreService.kt
+++ src/main/kotlin/mate/academy/StoreService.kt
@@ -4,5 +4,10 @@ class StoreService {
     // Function to update the store's stock with `with`
     fun updateStoreStock(store: Store, extraShoes: Int, extraShirts: Int, extraJackets: Int) {
         // TODO: implement
+        with(store) {
+            shoes += extraShoes
+            shirts += extraShirts
+            jackets += extraJackets
+        }
     }
 }
