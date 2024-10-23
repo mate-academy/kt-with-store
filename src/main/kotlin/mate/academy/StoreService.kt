dc7bb976c6f4468cf9dddb562e6c2b4b694ceca8
ilko-ilya <samilyakilya@gmail.com>
2024-10-23 14:10:48 +0300

    First commit

--- src/main/kotlin/mate/academy/StoreService.kt
+++ src/main/kotlin/mate/academy/StoreService.kt
@@ -3,6 +3,10 @@ package mate.academy
 class StoreService {
     // Function to update the store's stock with `with`
     fun updateStoreStock(store: Store, extraShoes: Int, extraShirts: Int, extraJackets: Int) {
-        // TODO: implement
+        with(store) {
+            store.shoes += extraShoes
+            store.shirts += extraShirts
+            store.jackets += extraJackets
+        }
     }
 }
